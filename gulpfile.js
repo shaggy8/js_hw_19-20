b29b39145155f365a0149b6c2b15c02363b73108
Shaggy8 <rominus@bigmir.net>
2016-04-03 20:13:18 +0300

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+
+gulp.task('sass', function () {
+      return gulp.src('scss/**/*.scss')
+                 .pipe(sass().on('error', sass.logError))
+                 .pipe(autoprefixer({
+                    browsers: ['last 7 versions'],
+                    cascade: false
+                  }))
+                 .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', function(){
+      return gulp.src('css/**/*.css')
+                 .pipe(concat('styles-min.css'))
+                 .pipe(cleanCSS())
+                 .pipe(gulp.dest('build'));
+});
+
+gulp.task('minify-js', function(){
+      return gulp.src('js/*.js')
+                 .pipe(concat('scripts-min.js'))
+                 .pipe(uglify())
+                 .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', function(){
+      gulp.run('minify-css', 'minify-js');
+});
+
+gulp.task('watch', function(){
+      watch('scss/*', function(event, cb) {
+          gulp.start('sass');
+      });
+});
