768ea4c2974199194d632f9df50abaf2e4924a37
Shaggy8 <rominus@bigmir.net>
2016-04-07 19:55:34 +0300

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -38,19 +38,54 @@ jQuery(function () {
 
   $('.banners').click(function(event) {
     var $target = $(event.target);
-    var $panels = $('.banners__accordion-panel');
-    var $panelIcons = $('.banners__accordion-panel-icon');
-    var $panelOpened = $('.banners__accordion-panel--opened');
-    var $panelOpenedIcon = $('.banners__accordion-panel-icon--opened');
+    var elems = {
+      'all': {
+        'panel': '.banners__accordion-panel',
+        'icon': '.banners__accordion-panel-icon'
+      },
+      'opened': {
+        'panel': '.banners__accordion-panel--opened',
+        'icon': '.banners__accordion-panel-icon--opened'
+      }
+    }
 
-    if ( !$target.is('.banners__accordion-panel') ||
-        $target.is('.banners__accordion-panel--opened') ) return;
 
+    if ( $target.is(elems.opened.panel + ', ' + elems.opened.icon)
+        ||
+        !$target.is(elems.all.panel + ', ' + elems.all.icon) ) return;
+
+
+    var $panels = $(elems.all.panel);
+    var $panelIcons = $(elems.all.icon);
+    var $panelOpened = $(elems.opened.panel);
+    var $panelIconOpened = $(elems.opened.icon);
+    var $textBlocks = $('.banners__accordion-text');
+    var targetNum;
+    var openedNum = $panels.index($panelOpened);
+    var easing;
+
+    if ($target.hasClass('banners__accordion-panel')) {
+      targetNum = $panels.index($target);
+    } else {
+      targetNum = $panelIcons.index($target);
+    }
+
+    if (targetNum < openedNum) {
+      easing = 'easeOutBounce';
+    } else {
+      easing = 'easeOutQuart';
+    }
+
+
+    $panelIconOpened.text('+');
     $panels.removeClass('banners__accordion-panel--opened');
-    $panelOpenedIcon.text('+');
     $panelIcons.removeClass('banners__accordion-panel-icon--opened');
+    $textBlocks.slideUp('slow', easing);
 
-    
+    $panelIcons.eq(targetNum).text('-');
+    $panels.eq(targetNum).addClass('banners__accordion-panel--opened');
+    $panelIcons.eq(targetNum).addClass('banners__accordion-panel-icon--opened');
+    $textBlocks.eq(targetNum).slideDown('slow', easing);
   });
 
 });
